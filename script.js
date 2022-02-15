d7767e6f51c3b63fa8b940732c43f87551e3fcd1
originalbplusj <originalbplusj@yahoo.com>
2022-02-15 16:38:47 -0600

    Studio commit

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            json.sort(function (a, b) { return b.hoursInSpace - a.hoursInSpace });
+            console.log(json);
+            const div = document.getElementById("container");
+            let counter = 0;
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                </div>
+                `;
+
+                counter = counter + 1;
+            }
+
+
+
+            document.getElementById("count").innerHTML = `The count is ${counter}`;
+
+        });
+    });
+});
